af12e0d2fc24f4f7b89be74350bde5f8c4af17cf
ezecrz <otro.ezequielcruz@gmail.com>
2024-11-24 18:53:22 -0500

    tuto2.php

--- tuto2.php
+++ tuto2.php
@@ -1,9 +1,9 @@
 <?php
-require('../fpdf.php');
+require'../fpdf.php';
 
 class PDF extends FPDF
 {
-// Cabecera de página
+// Cabecera de pÃ¡gina
 function Header()
 {
 	// Logo
@@ -12,30 +12,31 @@ function Header()
 	$this->SetFont('Arial','B',15);
 	// Movernos a la derecha
 	$this->Cell(80);
-	// Título
+	// TÃ­tulo
 	$this->Cell(30,10,'Title',1,0,'C');
-	// Salto de línea
+	// Salto de lÃ­nea
 	$this->Ln(20);
 }
 
-// Pie de página
+// Pie de pÃ¡gina
 function Footer()
 {
-	// Posición: a 1,5 cm del final
+	// PosiciÃ³n: a 1,5 cm del final
 	$this->SetY(-15);
 	// Arial italic 8
 	$this->SetFont('Arial','I',8);
-	// Número de página
+	// NÃºmero de pÃ¡gina
 	$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 }
 }
 
-// Creación del objeto de la clase heredada
+// CreaciÃ³n del objeto de la clase heredada
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-	$pdf->Cell(0,10,'Imprimiendo línea número '.$i,0,1);
+for($i=1;$i<=40;$i++){
+	$pdf->Cell(0,10,'Imprimiendo lÃ­nea nÃºmero '.$i,0,1);
+}
 $pdf->Output();
 ?>
